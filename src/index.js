a8e0a8b28d883552f68d3c35d77340cd76b07b5a
Nguyenhuugiap <nguyenhuugiap4@gmail.com>
2018-10-12 10:02:22 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
+  import 'bootstrap/dist/css/bootstrap.min.css';
+import _ from 'lodash';
+import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import YTSearch from 'youtube-api-search';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const API_KEY = 'AIzaSyAAFHIey3jG86-6nMkwSfjvvyBnK3L8NmM';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 500);
+    return (
+      <Fragment>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <div class="row">
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+        </div>
+      </Fragment>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
