d8ccc64f8cc2f5c02294b87f24d8ffb702b14812
Nguyenhuugiap <nguyenhuugiap4@gmail.com>
2018-10-12 10:04:08 +0700

    Updates

---
+++ precache-manifest.8e86adba6ff6f4b112556d5a75f11ddf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-simple/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5fd52b967b633ae90165",
+    "url": "/react-simple/static/js/runtime~main.5fd52b96.js"
+  },
+  {
+    "revision": "f0e5fe13f843380d8691",
+    "url": "/react-simple/static/js/main.f0e5fe13.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/react-simple/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "f0e5fe13f843380d8691",
+    "url": "/react-simple/static/css/main.c6d75841.chunk.css"
+  },
+  {
+    "revision": "27c2368a2d76f244f6272f749a90ea61",
+    "url": "/react-simple/index.html"
+  }
+];
